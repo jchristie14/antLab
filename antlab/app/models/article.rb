7563bbd2d1f17bb272849dd0a8df402ac6a87e40
John Christie <omeara.christie@gmail.com>
2016-09-26 13:45:14 -0400

    Add carrierwave

--- antlab/app/models/article.rb
+++ antlab/app/models/article.rb
@@ -2,5 +2,6 @@ class Article < ApplicationRecord
 
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true
+	mount_uploader :image, ImageUploader
 
 end
--- antlab/db/schema.rb
+++ antlab/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926150914) do
+ActiveRecord::Schema.define(version: 20160926171017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160926150914) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "image"
     t.index ["user_id"], name: "index_articles_on_user_id", using: :btree
   end
 
